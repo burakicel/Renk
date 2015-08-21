7c524c01612f5fc376398355b87534fcc93cc59f
Burak Icel <burakicel96@gmail.com>
2015-08-20 22:16:25 -0400

    Added back button to item detail

--- app/src/main/java/com/burakicel/renk/ImageDetailActivity.java
+++ app/src/main/java/com/burakicel/renk/ImageDetailActivity.java
@@ -2,20 +2,23 @@ package com.burakicel.renk;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.app.Activity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-public class ImageDetailActivity extends Activity {
+public class ImageDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Get Window Dimensions
         DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
@@ -38,5 +41,15 @@ public class ImageDetailActivity extends Activity {
                 .bitmapConfig(Bitmap.Config.RGB_565)
                 .build();
         imageLoader.displayImage(GalleryActivity.mThumbIds[position] ,imageView, options);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
